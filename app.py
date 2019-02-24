0a4b2d7704af1e4629acb51e1fca22473fe3b3b7
Alexander <arshak23@gmail.com>
2019-02-24 11:09:15 +0100

    scrape for email

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
-#nodemon --exec python3 app.py
+import re
+import requests
+from bs4 import BeautifulSoup
 
-conversion = 1000
-conversion_rate = 0.015
+url = "http://byggomontageservice.se/kontakt/"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser');
 
-print("Conversion calc: ", conversion, " visitors ", conversion*conversion_rate, " conversions")
-
-
-
-my_dict = range(1, 5)
-
-
-if my_dict[0] == 1:
-    print("wtf")
-    print("en till grej")
-elif my_dict[0] == 2:
-    print("twa")
+if soup.select("a[href*=mailto]"):
+    email = str(soup.select("a[href*=mailto]")[0])
+    print (email[email.find('mailto:')+len('mailto:'):email.rfind('"')])
 else:
-    print("hepp")
+    print ("no mailto")
