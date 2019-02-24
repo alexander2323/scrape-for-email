06847163685316a234eab3f82e91eef6a91e4762
Alexander <arshak23@gmail.com>
2019-02-24 09:06:10 +0100

    if

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
-#nodemon --exec python3
+#nodemon --exec python3 app.py
 
-print("Hey man")
+conversion = 1000
+conversion_rate = 0.015
+
+print("Conversion calc: ", conversion, " visitors ", conversion*conversion_rate, " conversions")
+
+
+
+my_dict = range(1, 5)
+
+
+if my_dict[0] == 1:
+    print("wtf")
+    print("en till grej")
+elif my_dict[0] == 2:
+    print("twa")
+else:
+    print("hepp")
